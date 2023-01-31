f813c8cd08972db105c948180342f686341f7ede
Drew <30042619@student.toiohomai.ac.nz>
2023-01-31 16:04:28 +1300

    changed folder structure

---
+++ index.php
@@ -0,0 +1,13 @@
+<!-- Header -->
+<?php 
+    $title = "E-Commerce";
+require("header.php") ?>
+
+<!-- Main Content -->
+<div class="container">
+    <h1>Hello, world!</h1>
+</div>
+
+
+<!-- Footer -->
+<?php require("footer.php") ?>
